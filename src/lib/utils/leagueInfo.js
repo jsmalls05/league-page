eafbea1183071843e9fab625348c48fb89ac0829
James Smalls <James_smalls_2120@yahoo.com>
2023-10-14 18:07:26 -0400

    dynasty update

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -2,7 +2,7 @@
 export const leagueID = "984638833209716736"; // your league ID
 export const leagueName = "Dynasty"; // your league name
 export const dues = 100; // (optional) used in template constitution page
-export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
+export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
